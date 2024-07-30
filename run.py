2aa9245c9d96497a3413b7d4f2685fda7bb5eefb
Mark Garcia <mymgcode@gmail.com>
2024-07-30 08:26:47 -0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+import sys
+
+log_file_path = "deletion_errors.log"
+
+def log_error(message):
+	with open(log_file_path, 'a') as log_file:
+		log_file.write(message + '\n')
+def print_progress(processed, total):
+	progress = processed / total
+	bar_length = 40
+	block = int(round(bar_length * progress))
+	progress_text = f"\rProgress: [{'#' * block + '-' * (bar_length - block)}] {round(progress * 100, 2)}%"
+	print(progress_text, end='')
+def GrantFullAdmin(file_path):
+	try:
+		log_message = subprocess.check_output(f'icacls "{file_path}" /grant Administrators:F /T /C /Q', shell=True).decode('utf-8')
+		if "Failed processing 0 files" in log_message:
+			return True
+		else:
+			log_error(f"granting permissions {file_path}: {log_message}")
+	except Exception as e:
+		log_error(f"Failed to granting permissions for {file_path}: {e}")
+	return False
+def TakeOwnership(file_path):
+	# f'takeown /F "{file_path}" /R /D Y'
+	try:
+		log_message = subprocess.check_output(f'takeown /F "{file_path}"', shell=True).decode('utf-8')
+		if "SUCCESS:" in log_message:
+			return True
+		else:
+			log_error(f"ownership {file_path}: {log_message}")
+	except Exception as e:
+		log_error(f"Failed to take ownership for {file_path}: {e}")
+	return False
+def ProcessDirectoryPermissions(directory):
+	processed_items = 0
+	if not os.path.exists(directory):
+		log_error(f"The specified path does not exist: {directory}")
+		print(f"The specified path does not exist: {directory}")
+	
+	all_items = []
+	for root, dirs, files in os.walk(directory, topdown=False):
+		for file in files:
+			all_items.append(os.path.join(root, file))
+		for dir in dirs:
+			all_items.append(os.path.join(root, dir))
+	all_items.append(directory)
+	
+	total_items = len(all_items)
+	for root, dirs, files in os.walk(directory, topdown=False):
+		for file in files:
+			if file == log_file_path:
+				continue
+			file_path = os.path.join(root, file)
+			if not TakeOwnership(file_path) or not GrantFullAdmin(file_path):
+				log_error(f"Failed to process file: {file_path}")
+			else:
+				if not TakeOwnership(root) or not GrantFullAdmin(root):
+					log_error(f"Failed to process root directory: {root}")
+				if not TakeOwnership(file_path) or not GrantFullAdmin(file_path):
+					log_error(f"Failed to process file: {file_path}")
+			
+			processed_items += 1
+			print_progress(processed_items, total_items)
+		
+		for dir in dirs:
+			dir_path = os.path.join(root, dir)
+			if not TakeOwnership(dir_path) or not GrantFullAdmin(dir_path):
+				log_error(f"Failed to process directory: {dir_path}")
+			processed_items += 1
+			print_progress(processed_items, total_items)
+
+current_dir = os.getcwd()
+# directory = "G:\\FileOwnershipTool"
+print("Current working directory:", current_dir)
+ProcessDirectoryPermissions(current_dir)
