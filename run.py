12f2e017d269ab6141bb97ef649b00de2fa8b5e8
Mark Garcia <mymgcode@gmail.com>
2024-07-30 12:08:14 -0600

    Update run.py

--- run.py
+++ run.py
@@ -16,18 +16,13 @@ def print_progress(processed, total):
 def GrantFullAdmin(file_path):
 	# f'icacls "{file_path}" /grant Administrators:F /T /C /Q'
 	try:
-		command = f'icacls "{file_path}" /grant Administrators:F /C /Q'
+		command = f'icacls "{file_path}" /grant Administrators:F /T /C /Q'
 		result = subprocess.run(command, shell=True, capture_output=True, text=True)
 		if result.returncode == 0:
 			return True
 		else:
 			log_message = result.stderr
 			log_error(f"Error granting Administrators permissions on '{file_path}': {log_message}")
-		#log_message = subprocess.check_output(command, shell=True).decode('utf-8', errors='replace')
-		#if "Failed processing 0 files" in log_message:
-			#return True
-		#else:
-			#log_error(f"Granting permissions {file_path}: {log_message}")
 	except Exception as e:
 		log_error(f"Failed to granting permissions for {file_path}: {e}")
 	return False
@@ -41,28 +36,18 @@ def TakeOwnership(file_path):
 		else:
 			log_message = result.stderr
 			log_error(f"Error taking ownership on '{file_path}': {log_message}")
-		#log_message = subprocess.check_output(command, shell=True).decode('utf-8', errors='replace')
-		#if "SUCCESS:" in log_message or "CORRECTO:" in log_message:
-			#return True
-		#else:
-			#log_error(f"Ownership {file_path}: {log_message}")
 	except Exception as e:
 		log_error(f"Failed to take ownership for {file_path}: {e}")
 	return False
 def AddUser(file_path):
 	try:
 		command = f'icacls "{file_path}" /grant "%USERNAME%":(OI)(CI)F'
-		#log_message = subprocess.check_output(command, shell=True).decode('utf-8', errors='replace')
 		result = subprocess.run(command, shell=True, capture_output=True, text=True)
 		if result.returncode == 0:
 			return True
 		else:
 			log_message = result.stderr
 			log_error(f"Error adding user on '{file_path}': {log_message}")
-		#if "SUCCESS:" in log_message or "CORRECTO:" in log_message:
-			#return True
-		#else:
-			#log_error(f"Add User {file_path}: {log_message}")
 	except Exception as e:
 		log_error(f"Failed to take add user for {file_path}: {e}")
 	return False
